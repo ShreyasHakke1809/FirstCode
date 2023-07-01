7723f16906a6e05ba507144950c3a3305e1bb477
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-01 15:06:30 +0530

    HelloWorldCode

---
+++ HelloWorldProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorldProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

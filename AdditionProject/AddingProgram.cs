54d3276113616bfd8fdf785aedbe36685775c60e
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-01 15:17:10 +0530

    AdditionProgram

---
+++ AdditionProject/AddingProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionProject
+{
+    internal class Calculater
+    {
+        public void AdditionOfTwoNumbers()
+        {
+            Console.WriteLine("Please enter first number");
+            int firstNumb = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter second number");
+            int secondNumb = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum:" + (firstNumb + secondNumb));
+
+    }
+    }
+}
---
+++ AdditionProject/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AdditionProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Calculater calculation = new Calculater();
+            calculation.AdditionOfTwoNumbers();
+            Console.ReadLine();
+
+
+        }
+    }
+}
